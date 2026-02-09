5ce04f127cd5212deacd671315cb30074338609a
pxlrbt <22632550+pxlrbt@users.noreply.github.com>
2026-02-09 16:21:26 +0000

    Code style

--- src/DocsServiceProvider.php
+++ src/DocsServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace pxlrbt\LaravelDocs;
 
 use Illuminate\Support\Facades\Route;
-use pxlrbt\LaravelDocs\Commands\PublishDocsAssetsCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
