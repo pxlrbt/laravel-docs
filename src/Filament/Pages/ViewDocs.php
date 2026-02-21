81407658d5377bd3344692b92897b460bddcd21d
Dennis Koch <info@pixelarbeit.de>
2026-02-21 13:39:54 +0100

    Filament v4 support

--- src/Filament/Pages/ViewDocs.php
+++ src/Filament/Pages/ViewDocs.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace pxlrbt\LaravelDocs\Filament\Pages;
 
 use Filament\Pages\Page;
-use Filament\Support\Enums\MaxWidth;
+use Filament\Schemas\Schema;
+use Filament\Support\Enums\Width;
 use Illuminate\Contracts\Support\Htmlable;
 use Livewire\Attributes\Url;
 use pxlrbt\LaravelDocs\DocsContent;
@@ -16,8 +17,6 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ViewDocs extends Page
 {
-    protected static string $view = 'docs::filament.pages.view-docs';
-
     protected static ?string $slug = 'docs';
 
     #[Url(as: 'page', except: 'index')]
@@ -50,19 +49,9 @@ class ViewDocs extends Page
         return DocsPlugin::get()->getNavigationSort();
     }
 
-    public function getMaxContentWidth(): MaxWidth
+    public function getMaxContentWidth(): Width
     {
-        return MaxWidth::ScreenExtraLarge;
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getViewData(): array
-    {
-        return [
-            'docsContent' => $this->renderDocsContent(),
-        ];
+        return Width::ScreenExtraLarge;
     }
 
     protected function renderDocsContent(): DocsContent
@@ -88,5 +77,14 @@ class ViewDocs extends Page
     public function navigateTo(string $slug): void
     {
         $this->currentSlug = $slug;
+    }
+
+    public function content(Schema $schema): Schema
+    {
+        return $schema->components([
+            view('docs::filament.pages.view-docs', [
+                'docsContent' => $this->renderDocsContent(),
+            ]),
+        ]);
     }
 }
